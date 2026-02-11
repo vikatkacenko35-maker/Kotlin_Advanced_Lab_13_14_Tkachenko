a9a98da76b112cf0b749df873de222d4d91cd2a4
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-02-11 14:15:16 +0300

    Add extension properties and functions

--- src/Question.kt
+++ src/Question.kt
@@ -45,11 +45,19 @@ class Quiz{
         var answered: Int = 3
     }
 }
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+fun Quiz.StudentProgress.printProgressBar(){
+    repeat(Quiz.answered)  { print("▓") }
+    repeat(Quiz.total- Quiz.answered)  { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
 fun main(){
-
+    Quiz.printProgressBar()
 //    println(question1.answer)
 //    println(question2.answer)
 //    println(question3.answer)
 //    println(question1.toString())
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+    println(Quiz.progressText)
 }
