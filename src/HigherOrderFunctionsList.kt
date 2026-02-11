dc0727916a7ce60926b6b473b065a85116bb3dd3
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-02-11 14:57:42 +0300

    map() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -49,6 +49,13 @@ val cookies = listOf(
     )
 )
 fun main(){
+    val fullMenu = cookies.map {
+        "${it.name} - ${it.price}"
+    }
+    println("Полное меню")
+    fullMenu.forEach {
+        println(it)
+    }
     cookies.forEach {
         println("Пункт меню: ${it.name}")
     }
